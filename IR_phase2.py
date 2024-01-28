55c40de9a2be776332eaf9541ce94fc6a6eec02b
sara khatir <sarakhatir2001@gmail.cmo>
2024-01-28 13:07:41 +0330

    add some comments

--- IR_phase2.py
+++ IR_phase2.py
@@ -11,23 +11,31 @@ except IOError:
 
 #creating positional index dic
 positional_index_dic = {}
+# Iterate over each document in the preprocessed data
 for docID, doc in preprocessed_data.items():
+    # Iterate over each term and its position in the document
     for position, term in enumerate(doc['content']):
+        # Check if the term is not in the positional index dictionary
         if term not in positional_index_dic:
             positional_index_dic[term] = {}
+        # Check if the document ID is not in the positional index for the term
         if docID not in positional_index_dic[term]:
             positional_index_dic[term][docID] = {'count': 0, 'positions': []}
+
         positional_index_dic[term][docID]['count'] += 1
+        # Add the position to the list of positions for the term in the document
         positional_index_dic[term][docID]['positions'].append(position)
 
+        # Check if 'total' is not in the positional index for the term
+        # Initialize a sub-dictionary for the total count
         if 'total' not in positional_index_dic[term]:
             positional_index_dic[term]['total'] = {'count': 0}
         positional_index_dic[term]['total']['count'] += 1
 
 
 #print(positional_index_dic['مهر'])
-print(positional_index_dic['فوتبال']['1053'])
-print(positional_index_dic['مهر']['total'])
+#print(positional_index_dic['فوتبال']['1053'])
+#print(positional_index_dic['مهر']['total'])
 
 # save positional index dic as a JSON file
 output_file_path = '/Users/sara/Desktop/amirkabir/fall02-03/Information Retrieval/project/IR_data_news_12k_positional_index_dic.json'
